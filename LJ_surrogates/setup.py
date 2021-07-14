7949c4c3f1d619d6ad816e04451d4c165c9cd755
ocmadin <owen.madin@gmail.com>
2021-07-13 23:03:07 -0600

    Add setup.py

---
+++ LJ_surrogates/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='LJ_surrogate_modeling',
+    version='',
+    packages=[''],
+    url='https://github.com/ocmadin/LJ_surrogates',
+    license='',
+    author='owenmadin',
+    author_email='owen.madin@colorado.edu',
+    description=''
+)
