343a821e65ce48dbb1f19346d766d55d2c9d34e9
Owen Madin <owenmadin@lilac-ln02.hpc.private>
2021-07-14 15:42:09 -0400

    LSF changes

--- projects/alcohol_alkane_test/prepare_simulations.py
+++ projects/alcohol_alkane_test/prepare_simulations.py
@@ -4,7 +4,7 @@ import pandas
 from openff.evaluator.datasets import PhysicalPropertyDataSet
 import shutil
 forcefield = 'openff-1-3-0.offxml'
-num_samples = 2
+num_samples = 4
 output_directory = 'modified_force_fields'
 data_filepath = 'alcohol_alkane_datapoints_test.csv'
 data_csv = pandas.read_csv(data_filepath)
@@ -20,3 +20,4 @@ for folder in os.listdir(output_directory):
     shutil.copy2('server-config.json', os.path.join(output_directory, folder))
     shutil.copy2('estimation-options.json', os.path.join(output_directory, folder))
     shutil.copy2('submit.sh', os.path.join(output_directory, folder))
+    shutil.copy2('benchmark.json', os.path.join(output_directory,folder))
