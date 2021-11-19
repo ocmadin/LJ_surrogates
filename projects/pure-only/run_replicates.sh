9f970d971a4bce2ba4c6c455528833e11cda9265
Owen Madin <owenmadin@lilac-ln02.hpc.private>
2021-11-19 18:33:20 -0500

    correct conda envs

--- projects/pure-only/run_replicates.sh
+++ projects/pure-only/run_replicates.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-conda activate lj-surrogates-mcmc
+conda activate LJ_surrogates
 
-number_of_replicates=40
+number_of_replicates=1
 
-python prepare_simulations.py -n $number_of_replicates -o 'modified_force_fields' -f 'openff-1-3-0.offxml' -d 'pure-alcohols.csv'
+python prepare_simulations.py -n $number_of_replicates -o 'modified_force_fields' -f 'openff-1-3-0.offxml' -d 'pure-only.csv'
 
 cd run_server
 #bsub < server-submit.sh
