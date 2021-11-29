ecbee045d899da396b601887540f1d4b3e057cb1
Owen Madin <owenmadin@lilac-ln02.hpc.private>
2021-11-29 13:44:17 -0500

    Fixed memory limit

--- projects/pure-only/run_replicates.sh
+++ projects/pure-only/run_replicates.sh
@@ -2,12 +2,12 @@
 
 conda activate LJ_surrogates
 
-number_of_replicates=1
+number_of_replicates=50
 
 python prepare_simulations.py -n $number_of_replicates -o 'modified_force_fields' -f 'openff-1-3-0.offxml' -d 'pure-only.csv'
 
 cd run_server
-#bsub < server-submit.sh
+bsub < server-submit.sh
 cd ..
 
 
