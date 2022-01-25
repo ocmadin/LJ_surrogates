18c6de14c449a74abd101a82ee10572f0e38bd50
Owen Madin <owenmadin@lilac-ln02.hpc.private>
2022-01-25 14:11:05 -0500

    LILAC compatibility changes

--- projects/pure-only/iterative/run_replicates.sh
+++ projects/pure-only/iterative/run_replicates.sh
@@ -2,9 +2,9 @@
 
 conda activate LJ_surrogates
 
-number_of_replicates=100
+number_of_replicates=10
 
-python prepare_simulations.py -n $number_of_replicates -o 'modified_force_fields' -f 'openff-1.0.0.offxml' -d 'pure-only.csv'
+python prepare_simulations.py -n $number_of_replicates -o 'modified_force_fields' -f 'iterative-ff-1.offxml' -d 'pure-only.csv'
 
 cd run_server
 bsub < server-submit.sh
