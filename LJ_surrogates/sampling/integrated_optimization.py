a207301df6b3980ecdadc71fb12c783fb4d96c8b
owenmadin <owen.madin@colorado.edu>
2022-02-15 09:38:12 -0700

    Change the way that server setup works

--- LJ_surrogates/sampling/integrated_optimization.py
+++ LJ_surrogates/sampling/integrated_optimization.py
@@ -59,19 +59,19 @@ class IntegratedOptimizer:
         os.makedirs(os.path.join(self.force_field_directory, str(self.n_simulations + 1)))
         forcefield = ForceField(self.force_field_source)
         lj_params = forcefield.get_parameter_handler('vdW', allow_cosmetic_attributes=True)
-        for j in range(df.shape[1]):
+        for j in range(df.shape[0]):
             smirks = df.columns[j].split('_')[0]
             param = df.columns[j].split('_')[1]
             for lj in lj_params:
                 if lj.smirks == smirks:
                     if param == 'epsilon':
-                        lj.epsilon = df.values[j] * unit.kilocalorie_per_mole
+                        lj.epsilon = df.values[0,j] * unit.kilocalorie_per_mole
                     elif param == 'rmin':
-                        lj.rmin_half = df.values[j] * unit.angstrom
+                        lj.rmin_half = df.values[0,j] * unit.angstrom
         forcefield.to_file(os.path.join('optimized_ffs', str(self.n_simulations + 1), 'force-field.offxml'))
         shutil.copy2('test-set-collection.json', os.path.join(self.force_field_directory, str(self.n_simulations + 1)))
 
-    def create_server(self, n_workers=10, cpus_per_worker=1, gpus_per_worker=1, port=8001):
+    def setup_server(self, n_workers=10, cpus_per_worker=1, gpus_per_worker=1, port=8001):
 
 
 
@@ -140,66 +140,65 @@ class IntegratedOptimizer:
 
 
     def submit_requests(self, folder_path, folder_list):
-        with self.lsf_backend:
-            with EvaluatorServer(calculation_backend=self.lsf_backend,
-                             working_directory=self.working_directory,
-                             port=self.port,
-                             enable_data_caching=False,
-                             delete_working_files=True):
+        with EvaluatorServer(calculation_backend=self.lsf_backend,
+                         working_directory=self.working_directory,
+                         port=self.port,
+                         enable_data_caching=False,
+                         delete_working_files=True):
 
-                from time import sleep
-                from openff.evaluator.client import RequestResult
-                requests = {}
-                os.makedirs('estimated_results', exist_ok=True)
-                self.results_directory = 'estimated_results'
+            from time import sleep
+            from openff.evaluator.client import RequestResult
+            requests = {}
+            os.makedirs('estimated_results', exist_ok=True)
+            self.results_directory = 'estimated_results'
 
-                for subdir in os.listdir(self.force_field_directory):
-                    if subdir in folder_list:
-                        if os.path.exists(os.path.join(folder_path, 'test-set-collection.json') and os.path.exists(
-                                os.path.join(folder_path, 'force-field.offxml'))):
-                            forcefield = ForceField.from_path(
-                                os.path.join(self.force_field_directory, subdir, 'force-field.offxml'))
-                            property_dataset = PhysicalPropertyDataSet.from_json(
-                                os.path.join(self.force_field_directory, subdir, 'test-set-collection.json'))
-                        else:
-                            raise ValueError('Folder for request must supply test-set-collection.json and force-field.offxml')
+            for subdir in os.listdir(self.force_field_directory):
+                if subdir in folder_list:
+                    if os.path.exists(os.path.join(folder_path, 'test-set-collection.json') and os.path.exists(
+                            os.path.join(folder_path, 'force-field.offxml'))):
+                        forcefield = ForceField.from_path(
+                            os.path.join(self.force_field_directory, subdir, 'force-field.offxml'))
+                        property_dataset = PhysicalPropertyDataSet.from_json(
+                            os.path.join(self.force_field_directory, subdir, 'test-set-collection.json'))
+                    else:
+                        raise ValueError('Folder for request must supply test-set-collection.json and force-field.offxml')
 
-                    if self.n_simulations > self.max_simulations:
-                        raise ValueError(
-                            f"Unable to request more than {self.max_simulations} simulations.  Please increase the maximum number of simulations")
+                if self.n_simulations > self.max_simulations:
+                    raise ValueError(
+                        f"Unable to request more than {self.max_simulations} simulations.  Please increase the maximum number of simulations")
 
-                    requests[subdir] = self.create_request(property_dataset, forcefield)
-                    forcefield.to_force_field().to_file(
-                        os.path.join('estimated_results', 'force_field_' + str(subdir) + '.offxml'))
-                while len(requests) > 0:
+                requests[subdir] = self.create_request(property_dataset, forcefield)
+                forcefield.to_force_field().to_file(
+                    os.path.join('estimated_results', 'force_field_' + str(subdir) + '.offxml'))
+            while len(requests) > 0:
 
-                    has_finished = set()
+                has_finished = set()
 
-                    for subdir, request in requests.items():
+                for subdir, request in requests.items():
 
-                        response, error = request.results(synchronous=False)
+                    response, error = request.results(synchronous=False)
 
-                        if (
-                                isinstance(response, RequestResult) and
-                                len(response.queued_properties) > 0
-                        ):
-                            # Some properties not estimated yet.
-                            continue
+                    if (
+                            isinstance(response, RequestResult) and
+                            len(response.queued_properties) > 0
+                    ):
+                        # Some properties not estimated yet.
+                        continue
 
-                        elif isinstance(response, RequestResult):
-                            # ALL PROPERTIES WERE SUCCESSFULLY ESTIMATED
-                            response.json(
-                                os.path.join('estimated_results', 'estimated_data_set_' + str(subdir) + '.json'))
+                    elif isinstance(response, RequestResult):
+                        # ALL PROPERTIES WERE SUCCESSFULLY ESTIMATED
+                        response.json(
+                            os.path.join('estimated_results', 'estimated_data_set_' + str(subdir) + '.json'))
 
-                        else:
-                            print(f"{subdir} FAILED - {error} {response}")
+                    else:
+                        print(f"{subdir} FAILED - {error} {response}")
 
-                        has_finished.add(subdir)
+                    has_finished.add(subdir)
 
-                    for subdir in has_finished:
-                        requests.pop(subdir)
+                for subdir in has_finished:
+                    requests.pop(subdir)
 
-                    sleep(60)
+                sleep(60)
 
     def create_request(self, property_dataset, forcefield):
 
@@ -251,41 +250,43 @@ class TestOptimizer(IntegratedOptimizer):
 
         self.max_simulations = 10
 
-        self.create_server(n_workers=10, cpus_per_worker=1, gpus_per_worker=1, port=self.port)
-        self.param_range = param_range
-        self.smirks = ['[#18:1]']
-        n_samples = 5
+        self.setup_server(n_workers=10, cpus_per_worker=1, gpus_per_worker=1, port=self.port)
+        with self.lsf_backend:
 
-        self.prepare_initial_simulations(n_samples=n_samples, smirks=self.smirks, relative_bounds=param_range)
+            self.param_range = param_range
+            self.smirks = ['[#18:1]']
+            n_samples = 5
 
-        folder_list = [str(i) for i in range(n_samples)]
+            self.prepare_initial_simulations(n_samples=n_samples, smirks=self.smirks, relative_bounds=param_range)
 
-        self.submit_requests(folder_path=self.force_field_directory, folder_list=folder_list)
+            folder_list = [str(i) for i in range(n_samples)]
 
-        self.build_physical_property_surrogate()
+            self.submit_requests(folder_path=self.force_field_directory, folder_list=folder_list)
 
-        self.objective = ConstrainedGaussianObjectiveFunction(self.dataplex.multisurrogate, self.dataplex.properties,
-                                                              self.dataplex.initial_parameters, 0.01)
-        self.objective.flatten_parameters()
-        iter = 0
-        objectives = []
-        params = []
-        while self.n_simulations <= self.max_simulations:
-            self.logger.info(
-                f'Optimization Iteration {iter}: optimizing over a surrogate built from {self.n_simulations} datasets')
-            bounds = []
-            for column in self.dataplex.parameter_values.columns:
-                minbound = min(self.dataplex.parameter_values[column].values)
-                maxbound = max(self.dataplex.parameter_values[column].values)
-                bounds.append((minbound, maxbound))
+            self.build_physical_property_surrogate()
 
-            result = differential_evolution(self.objective, bounds)
-            self.logger.info(
-                f'Optimization Iteration {iter} complete: Objective function value of {result.fun} and parameters of {result.x}')
-            objectives.append(result.fun)
-            params.append(result.x)
-            iter += 1
+            self.objective = ConstrainedGaussianObjectiveFunction(self.dataplex.multisurrogate, self.dataplex.properties,
+                                                                  self.dataplex.initial_parameters, 0.01)
+            self.objective.flatten_parameters()
+            iter = 0
+            objectives = []
+            params = []
+            while self.n_simulations <= self.max_simulations:
+                self.logger.info(
+                    f'Optimization Iteration {iter}: optimizing over a surrogate built from {self.n_simulations} datasets')
+                bounds = []
+                for column in self.dataplex.parameter_values.columns:
+                    minbound = min(self.dataplex.parameter_values[column].values)
+                    maxbound = max(self.dataplex.parameter_values[column].values)
+                    bounds.append((minbound, maxbound))
 
-            self.prepare_single_simulation(params=[result.x], labels = self.dataplex.parameter_labels)
-            self.submit_requests(folder_path=self.force_field_directory, folder_list=[str(self.n_simulations + 1)])
-        self.evaluator_server.stop()
+                result = differential_evolution(self.objective, bounds)
+                self.logger.info(
+                    f'Optimization Iteration {iter} complete: Objective function value of {result.fun} and parameters of {result.x}')
+                objectives.append(result.fun)
+                params.append(result.x)
+                iter += 1
+
+                self.prepare_single_simulation(params=[result.x], labels = self.dataplex.parameter_labels)
+                self.submit_requests(folder_path=self.force_field_directory, folder_list=[str(self.n_simulations + 1)])
+            self.evaluator_server.stop()
